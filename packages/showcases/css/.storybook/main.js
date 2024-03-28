312239005abe4f6709bb966cb6689234c0a95b72
Guillaume Casbas <guillaume.casbas@medelse.com>
2024-03-28 15:25:51 +0100

    chore: updgrade css showcase to storybook 8

--- packages/showcases/css/.storybook/main.js
+++ packages/showcases/css/.storybook/main.js
@@ -1,15 +1,16 @@
+import { dirname, join } from "path";
 /** @type { import('@storybook/html-webpack5').StorybookConfig } */
 const config = {
   stories: [
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
-    name: "@storybook/html-webpack5",
+    name: getAbsolutePath("@storybook/html-webpack5"),
     options: {
       builder: {
         useSWC: true,
@@ -21,3 +22,7 @@ const config = {
   },
 };
 export default config;
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
