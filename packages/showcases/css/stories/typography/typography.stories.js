0cca7e6e19d6e850893ea307a8961e2fdd37c1b8
Guillaume Casbas <guillaume.casbas@medelse.com>
2024-04-02 20:53:40 +0200

    wip

---
+++ packages/showcases/css/stories/typography/typography.stories.js
@@ -0,0 +1,17 @@
+export default {
+    title: 'Typography',
+};
+
+export const Example = () => `
+    <div>
+        <h1 class="title-1">Title 1</h1>
+        <div class="title-1">Title 1 (div)</div>
+        <h2 class="title-2">Title 2</h2>
+        <div class="title-2">Title 2 (div)</div>
+        <h3 class="title-3">Title 3</h3>
+        <div class="title-3">Title 3 (div)</div>
+        <h4 class="title-4">Title 4</h4>
+        <div class="title-4">Title 4 (div)</div>
+        <p>This is a regulat text</p>
+    </div>
+`;
